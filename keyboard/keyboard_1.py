1ecc7ec51f7d2d1f800e81412b6b12a8b18ec614
hhj7076-art <hhj7076@gmail.com>
2026-04-15 19:29:27 +0900

    1
    1

--- keyboard/keyboard_1.py
+++ keyboard/keyboard_1.py
@@ -1,5 +1,5 @@
 import curses
-from time import sleep
+from time import monotonic, sleep
 from gpiozero import Motor
 
 MOTOR1_PINS = (26, 19)
@@ -10,24 +10,25 @@ MOTOR4_PINS = (24, 23)
 # All motors use the exact same power value.
 MOTOR_POWER = 1.0
 DIRECTION_CHANGE_PAUSE = 0.06
+RELEASE_TIMEOUT = 0.2
 
 KEY_TO_MOTION = {
   ord("w"): (1, 1, 1, 1),
   ord("W"): (1, 1, 1, 1),
   ord("s"): (-1, -1, -1, -1),
   ord("S"): (-1, -1, -1, -1),
-  ord("a"): (1, 1, -1, -1),
-  ord("A"): (1, 1, -1, -1),
-  ord("d"): (-1, -1, 1, 1),
-  ord("D"): (-1, -1, 1, 1),
+  ord("a"): (1, 1, 0, 0),
+  ord("A"): (1, 1, 0, 0),
+  ord("d"): (0, 0, 1, 1),
+  ord("D"): (0, 0, 1, 1),
   ord(" "): (0, 0, 0, 0),
 }
 
 MOTION_LABEL = {
   (1, 1, 1, 1): "Forward",
   (-1, -1, -1, -1): "Backward",
-  (1, 1, -1, -1): "Left Turn",
-  (-1, -1, 1, 1): "Right Turn",
+  (1, 1, 0, 0): "Left Turn",
+  (0, 0, 1, 1): "Right Turn",
   (0, 0, 0, 0): "Stop",
 }
 
@@ -90,7 +91,9 @@ def draw_screen(stdscr, msg):
 
 def main(stdscr):
   curses.curs_set(0)
+  stdscr.nodelay(True)
   msg = "Ready"
+  last_input_time = monotonic()
 
   while True:
     draw_screen(stdscr, msg)
@@ -99,16 +102,24 @@ def main(stdscr):
     if key == 27:
       break
 
-    motion = KEY_TO_MOTION.get(key)
-    if motion is None:
-      if 32 <= key <= 126:
-        msg = f"Unknown key: {chr(key)}"
-      else:
-        msg = f"Unknown key code: {key}"
-      continue
+    now = monotonic()
 
-    apply_motion(motion)
-    msg = MOTION_LABEL[motion]
+    if key != -1:
+      last_input_time = now
+      motion = KEY_TO_MOTION.get(key)
+      if motion is None:
+        if 32 <= key <= 126:
+          msg = f"Unknown key: {chr(key)}"
+        else:
+          msg = f"Unknown key code: {key}"
+      else:
+        apply_motion(motion)
+        msg = MOTION_LABEL[motion]
+    elif current_motion != (0, 0, 0, 0) and now - last_input_time >= RELEASE_TIMEOUT:
+      apply_motion((0, 0, 0, 0))
+      msg = "Stop"
+
+    sleep(0.01)
 
   stop_all()
 
